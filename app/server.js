5d23c520228bc6ce58ec9beb48d3a55699caea3d
Kevin thapa <bobmarley9461238@gmail.com>
2025-12-07 12:34:24 +0000

    Implemented task deletion and improved error handling

--- app/server.js
+++ app/server.js
@@ -14,20 +14,53 @@ const pool = new Pool({
 });
 
 app.get("/tasks", async(req, res) => {
+    try {
     const result = await pool.query("SELECT * FROM tasks");
     const newFormat = result.rows.map(task => task.text).join("\n");
     res.set('Content-Type', 'text/plain');
     res.send(newFormat);
+
+    } catch (error) {
+        console.error("Error viewing the tasks: ", error);
+        return res.status(500).send({message: "Error outputting the tasks"})
+    }
 })
 
-app.post("/tasks", async(req, res) => {
-    const { text } = req.body;
+app.delete("/tasks/:id", async(req, res) => {
+    try {
+        const taskId = req.params.id;
+        
+        const result = await pool.query(
+            "DELETE FROM tasks WHERE id = $1 RETURNING *",
+            [taskId]
+        );
 
-    const result = await pool.query(
+        if (result.rowCount === 0) {
+            return res.status(404).send({message: "Task not found."})
+        }
+        return res.status(204).send();
+
+    } catch (error) {
+        console.error("Error deleting task: ", error);
+        return res.status(500).send({message: "Error deleting the task."});
+    }
+});
+
+app.post("/tasks", async(req, res) => {
+
+    try {
+        const { text } = req.body;
+        
+        const result = await pool.query(
         "INSERT INTO tasks(text) VALUES($1) RETURNING *",
         [text]
     );
-    res.status(201).json(result.rows[0]);
+        res.status(201).json(result.rows[0]);
+
+    } catch (error){
+        console.error("Task cannot be added: ", error);
+        return res.status(500).send({message: "Error adding the task."})
+    }
 });
 
 app.listen(3000, () => {
